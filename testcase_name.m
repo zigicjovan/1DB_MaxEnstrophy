b1ea8db1bf60b9ac5ea042a4b9fc320709b55617
Jovan Zigic <154387347+zigicjovan@users.noreply.github.com>
2024-08-07 23:10:24 -0400

    Add files via upload

--- testcase_name.m
+++ testcase_name.m
@@ -6,7 +6,7 @@ function [testcase_max,testcase1,testcase0] = testcase_name(caseID,N,stepscale,a
                 testcase_max = [ '_' num2str(N) '_LuMax_t' num2str(stepscale) '_' ascent '_L' num2str(Lip) '_K' num2str(cond) '' caseID ];%'p' num2str(caseID) '' ];
                 testcase1 = [ '_' num2str(N) '_LuCont1_t' num2str(stepscale) '_' ascent '_L' num2str(Lip) '_K' num2str(cond) '' caseID ];%'p' num2str(caseID) '' ];
                 testcase0 = [ '_' num2str(N) '_LuCont0_t' num2str(stepscale) '_' ascent '_L' num2str(Lip) '_K' num2str(cond) '' caseID ];%'p' num2str(caseID) '' ];
-            case {'RCGPR','RCGRMIL'}
+            case {'RCGPR','RCGRMIL','RSCG','RMSCG','RDSCG','RJSCG'}
                 testcase_max = [ '_' num2str(N) '_LuMax_t' num2str(stepscale) '_' ascent '' caseID ];
                 testcase1 = [ '_' num2str(N) '_LuCont1_t' num2str(stepscale) '_' ascent '' caseID ];
                 testcase0 = [ '_' num2str(N) '_LuCont0_t' num2str(stepscale) '_' ascent '' caseID ];
